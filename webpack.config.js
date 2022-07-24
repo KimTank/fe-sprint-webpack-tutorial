43c5b60940635a5fa30d42539e9fd0eb74cd8a1c
Hong Shik Branden Kim <kimploo@gmail.com>
2022-07-24 14:23:12 +0900

    tutorial 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: "./src/index.js",
@@ -15,4 +16,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "src", "index.html")
+  })]
 };
