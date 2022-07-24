3d19ebe0d7f438a4ccace8efe4285d1802b4f8be
Hong Shik Branden Kim <kimploo@gmail.com>
2022-07-24 12:18:30 +0900

    tutorial 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+      },
+    ],
   },
 };
